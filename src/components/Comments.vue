fa94583a709f5e62ea868a7dde3943e00dc7a8fd
João Guilherme Braga <jobraga@uolinc.com>
2021-06-28 14:23:56 -0300

    final

---
+++ src/components/Comments.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <h1>Comentários</h1>
+    <FormTodo v-on:add-todo="addComment"></FormTodo>
+    <div class="list-group-item" v-for="(comment, index) in comments" v-bind:key="index">
+      <span class="comment_author"><strong>Author: </strong>{{ comment.name }}</span>
+      <p>{{ comment.message}}</p>
+      <div>
+        <a href="#" title="Excluir" v-on:click.prevent="removeComment(index)">Exlcuir</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+
+<script>
+  import FormTodo from './FormTodo';
+
+  export default{
+      components:{
+        FormTodo
+      },
+      data(){
+        return{
+          comments: []
+        }
+      },
+      methods:
+      {
+        addComment(comment){
+          this.comments.push(comment)
+        }
+      },
+      watch:{
+        comments(val)
+        {
+          console.log("aqui: ", val)
+        }
+      }
+  }
+</script>
---
+++ src/components/FormTodo.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="form-todo form-group">
+    <p>
+      <input type="text" name="author" placeholder="nome" class="form-control" v-model="name">
+    </p>
+    <p>
+      <textarea placeholder="comentário" name="message" class="form-control" v-model="message"></textarea>
+    </p>
+    <button v-on:click="addComment" type="submit" class="btn btn-primary">Comentar</button>
+  </div>
+</template>
+
+
+<script>
+  export default{
+      data(){
+        return{
+          comments: [],
+          name: '',
+          message: ''
+        }
+      },
+      methods:
+      {
+        addComment()
+        {
+          if(this.name.trim() === '' || this.message.trim() === '')
+          {
+            alert('insira todas as informações');
+            return;
+          }
+
+          // passando evento
+          this.$emit('add-todo', {
+            name: this.name,
+            message: this.message
+          })
+
+          this.name = '';
+          this.message = '';
+        },
+        removeComment(index)
+        {
+          this.comments.splice(index, 1)
+        }
+      }
+    }
+</script>
